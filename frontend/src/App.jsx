ea1861c2ab498c0e05d3d394a404f83f5c140900
Pratik Kamble <pratikworkadd@gmail.com>
2025-04-28 17:22:51 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+}
+
+export default App;
